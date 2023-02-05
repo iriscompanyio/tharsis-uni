3c4183d1812679fc49de912e39805cabc08efdba
dany0814 <jcanahuirev@gmail.com>
2023-02-05 16:15:07 -0500

    Create Mvideo component and add in App.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MLabel from "./components/MLabel.vue";
 import MNavbar from "./components/MNavbar.vue";
+import MVideo from "./components/MVideo.vue";
 </script>
 
 <template>
     <MLabel/>
     <MNavbar />
+    <MVideo />
 </template>
 
 <style scoped>
---
+++ src/components/MVideo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="max-w-7xl w-full mx-auto py-16 flex flex-col items-center bg-black-500">
+      <div class="mx-auto text-center max-w-md mb-10">
+        <p class="mb-5 <sm:(!text-6xl mb-10)">
+          Our current mission
+        </p>
+        <p class="text-white-500 font-semibold"
+          >This year we are competing worldwide in NASA Human Exploration Rover Challenge (HERC) 2023.</p
+        >
+      </div>
+      <div class="grid md:grid-cols-2 gap-8 mt-8 w-full h-sm">
+        <div class="border-2 border-white aspect-video w-full rounded-xl h-full md:h-auto">
+          <iframe
+            class="w-full h-full"
+            :src="`https://www.youtube.com/embed/kmV5IDXUc9A`"
+            title="Diseño Mecánico Trabajando | Mechanical design"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        </div>
+        <div class="border-2 border-white aspect-video w-full rounded-xl h-full md:h-auto">
+          <iframe
+            class="w-full h-full"
+            :src="`https://www.youtube.com/embed/Sc-oLX-Pwns`"
+            title="Diseño Mecánico Trabajando | Mechanical design"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+</template>
+
+// class="grid md:grid-cols-2 gap-8 mt-8 w-full"
