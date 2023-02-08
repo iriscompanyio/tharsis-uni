7bee2005fca91305e40901bfd475e709c1ee616f
Diego <diegocubilla2001@gmail.com>
2023-02-08 16:01:17 -0300

    feat: add MFooter and MInitButton components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
-import MLabel from "./components/MLabel.vue";
-import MNavbar from "./components/MNavbar.vue";
-import MVideo from "./components/MVideo.vue";
-import MCounter from "./components/MCounter.vue";
-import MBrand from "./components/MBrand.vue";
-//import MFooter from "./components/MFooter.vue";
+    import MLabel from "./components/MLabel.vue";
+    import MNavbar from "./components/MNavbar.vue";
+    import MVideo from "./components/MVideo.vue";
+    import MCounter from "./components/MCounter.vue";
+    import MBrand from "./components/MBrand.vue";
+    import MFooter from "./components/MFooter.vue";
+    import MInitButton from "./components/MInitButton.vue";
 </script>
-
 <template>
-    <MLabel/>
+    <MLabel />
     <MNavbar />
     <MBrand />
     <MCounter />
     <MVideo />
-//    <MFooter />
-</template>
+    <MFooter />
+    <MInitButton />
+</template>  
 
-<style scoped>
-</style>
