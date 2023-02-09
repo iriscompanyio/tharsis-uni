c0736949cd45f0b090dbd328b4fd4f28298e56de
dany0814 <jcanahuirev@gmail.com>
2023-02-08 19:14:32 -0500

    Update href in some components

--- src/components/MLabel.vue
+++ src/components/MLabel.vue
@@ -4,7 +4,7 @@
       <div class="flex mx-auto items-center">
         <p class="<2xl:text-xs text-white font-bold text-lg">Tharsis lovers, 2 months and 20 days left for NASA HERC 2023!</p>
         <a
-          href="#"
+          href="https://www.facebook.com/tharsisUNI"
           class="<xl:hidden px-2rem ml-1rem text-black-600 bg-hex-A55488 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
           aria-current="page"
           >Learn more</a
--- src/components/MNavbar.vue
+++ src/components/MNavbar.vue
@@ -10,26 +10,26 @@
       </div>
       <div class="<2xl:ml-16px items-center ml-56px">
         <a
-          href="#"
+          href="#home"
           class="px-2rem text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
           aria-current="page"
           >Home</a
         >
 
         <a
-          href="#"
+          href="#mission"
           class="px-2rem text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
           >Events</a
         >
 
         <a
-          href="#"
+          href="#mission"
           class="px-2rem text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
           >Community</a
         >
 
         <a
-          href="#"
+          href="#mission"
           class="<sm:hidden px-2rem text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
           >Challenges</a
         >
@@ -42,7 +42,7 @@
           >English</a
         >
         <a
-          href="#"
+          href="#sponsors"
           class="px-2rem ml-1rem text-gray-600 bg-light-50 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
           aria-current="page"
           >Sponsor</a
